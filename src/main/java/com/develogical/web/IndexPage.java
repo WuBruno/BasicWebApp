994116b6f36e0a0524f885959cff5cb7e51ac395
Bruno Wu <brunowu2000@gmail.com>
2022-01-24 15:15:55 +0000

    Another small change

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,7 +7,7 @@ public class IndexPage extends HtmlPage {
   @Override
   protected void writeContentTo(PrintWriter writer) {
     writer.println(
-        "<h1>Welcome to Search!</h1>" +
+        "<h1>Welcome to Search again!</h1>" +
             "<p>Enter your query in the box below: " +
             "<form><input type=\"text\" name=\"q\" />" +
             "<input type=\"submit\">" +
